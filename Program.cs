9076b8472709724da97931318c069a1eec3d69a9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-20 20:24:20 -0300

    resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("---RAIO E TROVÃO---");
+
+double diferenca;
+double distancia;
+
+CalcularDistancia();
+
+void CalcularDistancia()
+{
+    Console.WriteLine("Insira a diferença de tempo entre o raio e o trovão...");
+    diferenca = Convert.ToDouble(Console.ReadLine());
+    
+    distancia = (diferenca * 340.29) / 1000;
+    Console.WriteLine("Aproximadamente: " + distancia.ToString("N2") + "km");
+}
